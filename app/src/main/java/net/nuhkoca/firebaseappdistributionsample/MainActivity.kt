d7d1f3c967f184844c046660aa7004e33f801e21
nuhkoca <nuhkocaa@gmail.com>
2019-10-28 01:30:34 +0300

    Add some code to layout side

--- app/src/main/java/net/nuhkoca/firebaseappdistributionsample/MainActivity.kt
+++ app/src/main/java/net/nuhkoca/firebaseappdistributionsample/MainActivity.kt
@@ -1,12 +1,15 @@
 package net.nuhkoca.firebaseappdistributionsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.tvVersion
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvVersion.text = "v ${BuildConfig.VERSION_NAME}"
     }
 }
